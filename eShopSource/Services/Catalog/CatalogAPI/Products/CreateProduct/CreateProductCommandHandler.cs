ac75d6520582a4ac183e59da1c892414a0f449ee
Praveenraj <praveenraj1982@gmail.com>
2026-01-04 20:39:55 +0530

    Section 5 Catalog API with MediatR,Carter Implemented

---
+++ eShopSource/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price): ICommand<CreateProductResult>;
+    public record CreateProductResult(
+        Guid Id);
+
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic goes here...
+
+            //create a new product entity from command object
+            var product = new Product
+            {
+
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save product to database (omitted for brevity) - TODO: Implement database save logic here
+
+            //return the result with the new product ID
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
