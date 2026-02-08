329f5f20001d036e848360dff07a2cd9f6812e45
Praveenraj <praveenraj1982@gmail.com>
2026-02-08 12:09:08 +0530

    Section 9 completed

---
+++ eShopSource/Services/Basket/Basket.API/Data/CacheBasketRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace Basket.API.Data;
+
+public class CacheBasketRepository(IBasketRepository basketRepository,
+    IDistributedCache cache)
+    : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+        if (!string.IsNullOrEmpty(cachedBasket))
+        {
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+        }
+
+        var basket = await basketRepository.GetBasketAsync(userName, cancellationToken);
+        await cache.SetStringAsync(userName, JsonSerializer.Serialize<ShoppingCart>(basket));
+
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart shoppingCart, CancellationToken cancellationToken = default)
+    {
+        await basketRepository.StoreBasketAsync(shoppingCart, cancellationToken);
+
+        await cache.SetStringAsync(shoppingCart.UserName, JsonSerializer.Serialize<ShoppingCart>(shoppingCart), cancellationToken);
+        return shoppingCart;
+    }
+    public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        await basketRepository.DeleteBasketAsync(userName, cancellationToken);
+
+        // Check if a cache entry exists first
+        var cached = await cache.GetStringAsync(userName, cancellationToken);
+        if (string.IsNullOrEmpty(cached))
+        {
+            // Nothing to remove from cache
+            return true;
+        }
+
+        await cache.RemoveAsync(userName, cancellationToken);
+        return true;
+    }
+}
--- eShopSource/Services/Basket/Basket.API/Program.cs
+++ eShopSource/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,7 @@
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,13 +21,34 @@ builder.Services.AddMarten(options =>
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
+// Repositories
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CacheBasketRepository>();
+
+//Cache services
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("BasketDB")!,
+        name: "BasketDB-Check",
+        tags: new[] { "ready" })
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!,
+        name: "Redis-Check",
+        tags: new[] { "ready" });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions { 
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
